3e0f2edf7255a1ce2c7535bc34b6087bd6cb1277
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-06-10 00:17:10 +0300

    created pagination,sidebar & base.html,tag_list_view

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from todo_list.views import TaskListView
+from .views import (
+    TaskListView,
+    TagListView
+)
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name='task-list')
+    path('', TaskListView.as_view(), name='task-list'),
+    path('tags/', TagListView.as_view(), name='tag-list')
 ]
 
 app_name = 'todo_list'
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from todo_list.models import Task
+from .models import Task, Tag
 
 
 class TaskListView(ListView):
     queryset = Task.objects.all().prefetch_related("tags")
+    paginate_by = 10
+
+
+class TagListView(ListView):
+    queryset = Tag.objects.all()
+    paginate_by = 10
